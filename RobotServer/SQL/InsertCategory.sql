9cf25c507b22f1eb42960d9ac2f010557ca12491
David J. Straley <djstraley@sandyhook.com>
2024-08-09 10:10:04 -0500

    Addded SQL Scripts which pre-populate the database with necessary data

---
+++ RobotServer/SQL/InsertCategory.sql
@@ -0,0 +1,23 @@
+USE [RobotShop]
+GO
+
+insert into dbo.Category
+(CategoryName)
+values
+('Heads')
+
+insert into dbo.Category
+(CategoryName)
+values
+('Bases')
+
+insert into dbo.Category
+(CategoryName)
+values
+('Arms')
+
+insert into dbo.Category
+(CategoryName)
+values
+('Torsos')
+
