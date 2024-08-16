56439e13e16c5b7cbf224291bc9463ff0148fab6
David J. Straley <djstraley@sandyhook.com>
2024-08-16 11:49:47 -0500

    fixed stored procedure to decrement the number of the specified item in cart instead of all of the items in the cart of the specified ProductID

--- RobotServer/SQL/StoredProcedures/dbo.CartItemDeleteByUserID-ProductID.sql
+++ RobotServer/SQL/StoredProcedures/dbo.CartItemDeleteByUserID-ProductID.sql
@@ -1,12 +1,13 @@
 USE [RobotShop]
 GO
 
-/****** Object:  StoredProcedure [dbo].[CartItemDeleteByUserID-ProductID]    Script Date: 8/14/2024 4:13:53 PM ******/
+/****** Object:  StoredProcedure [dbo].[CartItemDeleteByUserID-ProductID]    Script Date: 8/16/2024 11:43:28 AM ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
+
 
 
 /*
@@ -19,21 +20,38 @@ select @ProductID = 1
 exec dbo.[CartItemDeleteByUserID-ProductID] @UserID, @ProductID
 */
 
-CREATE OR ALTER   PROCEDURE [dbo].[CartItemDeleteByUserID-ProductID]
-	-- Add the parameters for the stored procedure here
+/* 
+This will decrement the number of ProductID items that there are in the
+cart for this User, until none are left.
+*/
+
+CREATE OR ALTER     PROCEDURE [dbo].[CartItemDeleteByUserID-ProductID]
 	@UserID as int,
 	@ProductID as int
 AS
 BEGIN
-	-- SET NOCOUNT ON added to prevent extra result sets from
-	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
+
 	declare @CartID int
+	declare @nCount int
+	declare @CartItemID int
+
 	select @CartID = CartID from Cart where UserID = @UserID
-	/* This removes all the items of type Product from the cart;
-	 * it does not just decrement the number of such items.
-	 */
-	delete from CartItem where CartID = @CartID and ProductID = @ProductID
+	select @nCount = count(*) from CartItem 
+		where CartID = @CartID
+		and ProductID = @ProductID
+	if(@nCount > 0)
+	Begin
+		/* decrement the number of this ProductID items that are in the cart
+		*/
+		select Top 1 @CartItemID = CartItemID 
+			from CartItem
+			where CartID = @CartID 
+			and ProductID = @ProductID 
+			order by CartItemID ASC
+		delete from CartItem where CartItemID = @CartItemID
+	End
 END
 GO
+
 
